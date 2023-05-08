90d8092a9b563f1df8477ec2b788a7c13e7be12f
jithin kg <jithin@jithins-MacBook-Air.local>
2023-05-08 14:25:43 +0530

    fix: service_test.go reflecrt initialize function changes

--- mhook/service_test.go
+++ mhook/service_test.go
@@ -12,42 +12,41 @@ import (
 func TestService(t *testing.T) {
 	// Create a test webhook
 	// Create a test webhook
-wh := &Webhook{
-	Address: "https://client.example.com",
-	Config: struct {
+	wh := &Webhook{
+		Address: "https://client.example.com",
+		Config: struct {
 			URL             string   `json:"url"`
 			ContentType     string   `json:"content_type"`
 			Secret          string   `json:"secret,omitempty"`
 			AlternativeURLs []string `json:"alt_urls,omitempty"`
-	}{
+		}{
 			URL:         "https://example.com/webhook",
 			ContentType: "application/json",
 			Secret:      "",
-	},
-	FailureURL: "https://failure.example.com",
-	Events:     []string{"event1", "event2"},
-	Matcher: struct {
+		},
+		FailureURL: "https://failure.example.com",
+		Events:     []string{"event1", "event2"},
+		Matcher: struct {
 			DeviceID []string `json:"device_id"`
-	}{
+		}{
 			DeviceID: []string{"device1", "device2"},
-	},
-	Duration: time.Second * 5,
-	Until:    time.Now().Add(time.Second * 5),
-}
-
-
-	cfg := &WatchConfig{
-		Webhooks:           []Webhook{*wh},
-		WatchUpdateInterval: time.Second * 5,
+		},
+		Duration: time.Second * 5,
+		Until:    time.Now().Add(time.Second * 5),
 	}
-// Create a sample function to handle webhook updates
+
+	// cfg := &WatchConfig{
+	// 	Webhooks:           []Webhook{*wh},
+	// 	WatchUpdateInterval: time.Second * 5,
+	// }
+	// Create a sample function to handle webhook updates
 	handleWebhookUpdate := func(webhooks []Webhook) {
 		fmt.Println("Webhook update:", webhooks)
 	}
 	// Create a test watch
 	testWatch := WatchFunc(handleWebhookUpdate)
 	// Initialize the service
-	service, cleanup, err := Initialize(cfg, testWatch)
+	service, cleanup, err := Initialize(testWatch)
 	require.NoError(t, err)
 	defer cleanup()
 
@@ -57,45 +56,46 @@ wh := &Webhook{
 
 	// Test retrieving webhooks for an owner
 	webhooks, err := service.AllWebhooks("owner1")
-	
-// assert.NoError(t, err)
 
-// 	require.NoError(t, err)
-// 	assert.Len(t, webhooks, 1)
-// 	assert.Equal(t, wh.Config.URL, webhooks[0].Config.URL)
-// Test adding a second webhook
-wh2 := &Webhook{
-	Address: "https://client2.example.com",
-	Config: struct {
+	// assert.NoError(t, err)
+
+	//	require.NoError(t, err)
+	//	assert.Len(t, webhooks, 1)
+	//	assert.Equal(t, wh.Config.URL, webhooks[0].Config.URL)
+	//
+	// Test adding a second webhook
+	wh2 := &Webhook{
+		Address: "https://client2.example.com",
+		Config: struct {
 			URL             string   `json:"url"`
 			ContentType     string   `json:"content_type"`
 			Secret          string   `json:"secret,omitempty"`
 			AlternativeURLs []string `json:"alt_urls,omitempty"`
-	}{
+		}{
 			URL:         "https://example.com/webhook2",
 			ContentType: "application/json",
 			Secret:      "",
-	},
-	FailureURL: "https://failure2.example.com",
-	Events:     []string{"event1", "event2"},
-	Matcher: struct {
+		},
+		FailureURL: "https://failure2.example.com",
+		Events:     []string{"event1", "event2"},
+		Matcher: struct {
 			DeviceID []string `json:"device_id"`
-	}{
+		}{
 			DeviceID: []string{"device1", "device2"},
-	},
-	Duration: time.Second * 5,
-	Until:    time.Now().Add(time.Second * 5),
-}
+		},
+		Duration: time.Second * 5,
+		Until:    time.Now().Add(time.Second * 5),
+	}
 
-err = service.Add("owner1", wh2)
-assert.NoError(t, err)
+	err = service.Add("owner1", wh2)
+	assert.NoError(t, err)
 
-// Test retrieving all webhooks for the owner
-webhooks, err = service.AllWebhooks("owner1")
-require.NoError(t, err)
-assert.Len(t, webhooks, 2)
+	// Test retrieving all webhooks for the owner
+	webhooks, err = service.AllWebhooks("owner1")
+	require.NoError(t, err)
+	assert.Len(t, webhooks, 2)
 
-// Sleep for a few seconds to allow the watch to update
-time.Sleep(time.Second * 10)
+	// Sleep for a few seconds to allow the watch to update
+	time.Sleep(time.Second * 10)
 
 }
